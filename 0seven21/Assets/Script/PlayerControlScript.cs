ce54e00a63f6051d9bca742012f3b2de3ca23d49
N133 <44888605+N133@users.noreply.github.com>
2018-11-28 12:48:48 +0900

    noha

---
+++ 0seven21/Assets/Script/PlayerControlScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+	}
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(1, 0, 0);
+        }
+    }
+}
