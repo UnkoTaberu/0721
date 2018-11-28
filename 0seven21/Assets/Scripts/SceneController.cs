6e3b9a6ddf770d9a4c188f84bf28b58ff9def2a3
potatoaa <44855353+potatoaa@users.noreply.github.com>
2018-11-28 14:39:57 +0900

    Revert "Merge branch 'mapmapttt'"
    This reverts commit e30e1a210acd15e3ed4c63cae47e77daad990443, reversing
    changes made to 1d4b0dfb0a9bbf9a00bd2c23f452c4431f23df8f.

--- 0seven21/Assets/Scripts/SceneController.cs
+++ 0seven21/Assets/Scripts/SceneController.cs
@@ -62,13 +62,10 @@ public class SceneController : MonoBehaviour
         {
             for (var y = 0; y < generator.height; y++)
             {
-
                 var tile = map[x, y] == 1 ? Instantiate(floorPrefab) : Instantiate(wallPrefab);
                 tile.transform.SetParent(tileContainer);
                 tile.transform.localPosition = new Vector2(x, y);
 
-<<<<<<< HEAD
-=======
                 // test
                 if (itemmap[x, y] == 3)
                 {
@@ -76,11 +73,8 @@ public class SceneController : MonoBehaviour
                     tile2.transform.SetParent(ItemtileContainer);
                     tile2.transform.localPosition = new Vector2(x, y);
                 }
->>>>>>> master
             }
         }
-
-        
 
         wallPrefab.gameObject.SetActive(false);
         floorPrefab.gameObject.SetActive(false);
